f6d8533d4bbb6b25da209af1c773a0445d6999a8
Jonas <jonasdh98@hotmail.com>
2018-06-13 21:25:15 +0200

    added navbar to signup.php
    - added navbar to signup.php

--- musicproject/website/signup.php
+++ musicproject/website/signup.php
@@ -4,11 +4,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="description" content="Halftime, Music, Producers">
+    <meta name="keywords" content="Half time, halftime, half, time, Producers, music, music kits, kits, free, pack">
+    <meta name="author" content="Jonas">
+
+    <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/signup.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+    <title>HalfTime</title>
 </head>
+
 <body>
-    <?php
-    print('hi'); 
-    ?>
+    <header>
+
+        <div class="navgrid">
+            <p></p>
+            <div class="title">
+                <a href="index.html">
+                    <i>HALF | TIME</i>
+                </a>
+            </div>
+            <div class="signup">
+                <a href="php/login.php">Log-In</a> |
+                <a href="php/signup.php">Sign-up</a>
+            </div>
+        </div>
+
+        <script src="js/js.js"></script>
+        <div class="container">
+            <div id="navgrid">
+                <div class="navlink">
+                    <a href="index.html">Home</a>
+                </div>
+                <div class="navlink">
+                    <a href="php/library.php">Library</a>
+                </div>
+                <div class="navlink">
+                    <a href="php/packs.php">Packs</a>
+                </div>
+                <div class="navlink">
+                    <a href="php/support.php">Support</a>
+                </div>
+            </div>
+        </div>
+    </header>
 </body>
 </html>
